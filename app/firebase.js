94b3c523954da3dfe87a9648a1d585d597633157
Chris Hyorok Lee <hl3838@columbia.edu>
2025-03-21 17:44:07 -0400

    fix the import for Firestore

--- app/firebase.js
+++ app/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,7 +19,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Analytics is optional and might not work in all environments
+let analytics = null;
+try {
+  analytics = getAnalytics(app);
+} catch (error) {
+  console.log("Analytics not available in this environment");
+}
 
 // Initialize Firestore
 const db = getFirestore(app);
