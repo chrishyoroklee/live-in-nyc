aeaef0bc7cc383798f263dd13b292b16d6b5fc90
Chris Hyorok Lee <hl3838@columbia.edu>
2025-03-20 21:42:15 -0400

    set up backend

---
+++ app/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDaLr9NcRJUqKUKt9Yor9ezyU1FjTs-2Ls",
+  authDomain: "livenyc-12314.firebaseapp.com",
+  projectId: "livenyc-12314",
+  storageBucket: "livenyc-12314.firebasestorage.app",
+  messagingSenderId: "608337819244",
+  appId: "1:608337819244:web:d5e6281bddcd905fb64a49",
+  measurementId: "G-W0BRSH5S0V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export { db };
